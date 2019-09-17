193e983059c5ce27a815156c6c78e698afd6b61b
Mathilda <tilda-3@hotmail.com>
2019-09-17 17:28:42 +0200

    Some changes

--- gulpfile.js
+++ gulpfile.js
@@ -15,55 +15,75 @@ const files = {
 
 // Copy HTML
 function copyHTML() {
+    // Find HTML path
     return src(files.htmlPath)
+        // Copy to folder 'Pub'
         .pipe(dest('pub')
     );
 }
 
 // Copy CSS
 function copyCSS() {
+    // Find CSS path
     return src(files.cssPath)
+        // Concat CSS files to one
         .pipe(concatCSS('main.css'))
+        // Minify CSS file
         .pipe(cleanCss())
+        // Copy to folder 'Pub'
         .pipe(dest('pub/css')
+        // Sending changes for livereload
         .pipe(browserSync.stream())
     );
 }
 
 // Copy JS
 function copyJS() {
+    // Find JS path
     return src(files.jsPath)
+        // Concat JS files to one
         .pipe(concat('main.js'))
+        // Minify JS file
         .pipe(uglify())
+        // Copy to folder 'Pub'
         .pipe(dest('pub/js')
     );
 }
 
 // Copy images
 function copyImages() {
+    // Find Images path
     return src(files.imagePath)
+        // Copy to folder 'Pub'
         .pipe(dest('pub')
     );
 }
 
 // Task: Watcher
 function watchTask() {
+    // Start BrowserSync
     browserSync.init({
         server: {
+            // Face to 'Pub' folder
             baseDir: 'pub/'
         }
     })
+    // Watch for files path
     watch([files.htmlPath,
         files.cssPath,
         files.jsPath,
         files.imagePath],
+        // Check for changes parallel from these functions
         parallel(copyHTML,
             copyCSS,
             copyJS,
             copyImages)
+    // When change = reload browser
     ).on('change', browserSync.reload);
 }
 
+// Export functions to public
+// First all functions parallel, then the watcher after
 exports.default = series(
     parallel(copyHTML,
         copyCSS,
